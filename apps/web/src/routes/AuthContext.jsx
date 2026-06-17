69071e3f26f693243f6417e819f80424506ed520
juliusmwass <juliusmwass100@gmail.com>
2026-06-17 09:50:35 +0300

    Added an AppRoutes page

---
+++ apps/web/src/routes/AuthContext.jsx
@@ -0,0 +1,48 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Scan from "../pages/Scan";
+import ScanDetails from "../pages/ScanDetails";
+import NotFound from "../pages/NotFound";
+
+const isAuthenticated = () => {
+  return localStorage.getItem("token");
+};
+
+function ProtectedRoute({ children }) {
+  return isAuthenticated() ? children : <Navigate to="/login" />;
+}
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      {/* PUBLIC */}
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* PROTECTED */}
+      <Route
+        path="/scan"
+        element={
+          <ProtectedRoute>
+            <Scan />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/scan/:scanId"
+        element={
+          <ProtectedRoute>
+            <ScanDetails />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
