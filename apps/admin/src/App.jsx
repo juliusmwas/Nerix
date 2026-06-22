0ae9def09d645deda10a5a03f60486d96298d391
juliusmwass <juliusmwass100@gmail.com>
2026-06-22 13:46:03 +0300

    Admin setup

---
+++ apps/admin/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 class="text-xl text-blue-700 font-bold underline">
+        Hello Welcome to the Admin Panel
+      </h1>
+    </>
+  );
+}
