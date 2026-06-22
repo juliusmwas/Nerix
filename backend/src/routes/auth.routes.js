430fca6d9799e23315095542de86c7cba82f1910
juliusmwass <juliusmwass100@gmail.com>
2026-06-22 13:00:01 +0300

    Updated the  auth routes to handle user reg and login

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,38 +1,25 @@
 /**
- * Authentication Routes
- *
- * Purpose:
- * - Define API endpoints for authentication
- * - Connect routes to controller functions
- *
- * Base path (to be set in app.js):
- * /api/auth
- */
+
+* Authentication Routes
+*
+* Base: /api/auth
+  */
 
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import {
+  register,
+  login,
+  logout,
+  getMe,
+} from "../controllers/auth.controller.js";
+
+import { authenticateUser } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-/**
- * Route: Register new user
- * Method: POST
- * Endpoint: /api/auth/register
- *
- * Body:
- * - fullName
- * - email
- * - password
- * - confirmPassword
- */
 router.post("/register", register);
-
-/**
- * Future routes (we will add soon):
- *
- * router.post("/login", login);
- * router.post("/logout", logout);
- * router.get("/me", authMiddleware, getMe);
- */
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/me", authenticateUser, getMe);
 
 export default router;
